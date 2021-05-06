4bf26275f387ab23276ed6c738bf27a1dfd90932
Sushant Mahadik <sushant.mahadik@tadigital.com>
2021-05-06 18:33:50 +0530

    Logout Functinality Added and Get Current User Id and Name (UserService added)

---
+++ BookStore/Helpers/CustomEmailTagHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace BookStore.Helpers
+{
+    public class CustomEmailTagHelper:TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a";
+            output.Attributes.SetAttribute("href", "mailto:msushant04@gmail.com");
+            output.Attributes.Add("id", "CustEmail");
+            output.Content.SetContent("my-email");
+        }
+    }
+}
